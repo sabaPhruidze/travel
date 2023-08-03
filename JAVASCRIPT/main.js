dde2b123e8c0942aa9ac4fed5dbf57057b220f23
Saba Phruidze <sabaphruidze01@gmail.com>
2023-08-03 09:56:49 +0400

    this project was made in here and then I tried to copy and make it more easily to read in react typescript

---
+++ JAVASCRIPT/main.js
@@ -0,0 +1,7 @@
+$('.places-card .location-main-photo').mouseenter(function () {
+$('.location-logo').css({"display": "none"});
+});
+$('.places-card .location-main-photo').mouseleave(function () {
+    $('.location-logo').css({"display": "inline"});
+    });
+    // class="location-main-photo"
